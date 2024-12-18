d74d113dcb7b2cd1b6d2e7edf757b33cef6224d6
AbdullahiAhm <157635160+AbdullahiAhm@users.noreply.github.com>
2024-12-18 15:19:17 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css'; // Import your CSS file
+import App from './App'; // Import your main App component
+
+ReactDOM.render(<App />, document.getElementById('root'));
